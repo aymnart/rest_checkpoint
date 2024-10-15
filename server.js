1e2bfa58527f8688f537d26f9fabc6f983728b17
believer297 <amaraymen5@gmail.com>
2024-10-15 10:53:40 +0100

    SECOND

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-console.log(process.env.PORT)
+const port = process.env.PORT;
+const app = express();
+
+
+
+
+app.listen(port, (err)=> err? console.error(err): console.log(`SERVER IS RUNNING ON PORT ${port}`) );
